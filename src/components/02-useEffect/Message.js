84571a33a62ccb912907cfb6ad0137faba310587
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-15 12:56:24 -0500

    prevent memory leak removing listeners on cleanup

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [cords, setCords] = useState({x: 0, y: 0});
+
+    const {x, y} = cords;
+
     // will triggered unmounted when component unmount
     useEffect(() => {
-        console.log("<Message /> mount!!")
+        // console.log("<Message /> mount!!");
+
+        const mouseMove = (e) => {
+            const cordenadas = { x: e.x, y: e.y };
+            // console.log( cordenadas );
+            console.log(" :D ")
+            setCords( cordenadas );
+        }
+
+        window.addEventListener('mousemove', mouseMove);
 
         return () => {
-            console.warn("<Message /> unmounted");
+            // console.warn("<Message /> unmounted");
+
+            // It's important to remove events listeners on cleanup
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, [])
 
     return (
         <div>
             <h1>Hola Jose!!</h1>
+            <pre>X: { x } - Y: { y }</pre>
         </div>
     )
 }
