81d2b91017c45e7e9cd171e38244668154047e29
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-17 18:29:34 -0500

    add styles todoApp

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import { todoReducer } from './todoReducer';
 
+import './styles.css';
+
 
 const initialState = [{
     id: new Date().getTime(),
@@ -17,15 +19,53 @@ export const TodoApp = () => {
             <h1>Todo APP</h1>
             <hr />
 
-            <ul>
-                {
-                    todos.map( ({id, desc}) => {
-                        return (
-                            <li key={ id }> { desc} </li>
-                        )
-                    })    
-                }
-            </ul>
+            <div className="row">
+
+                <div className="col-7">
+                    <ul className="todo-list">
+                        {
+                            todos.map( (item, i) => {
+                                return (
+                                    <div 
+                                        key={ item.id }
+                                        className="todo-item"
+                                    >
+                                        <p>{i+1}. { item.desc } </p>
+                                        <button
+                                            className="btn btn-outline-danger btn-sm"
+                                        >
+                                            Borrar
+                                        </button>
+                                    </div>
+                                )
+                            })    
+                        }
+                    </ul>
+                </div>
+
+                <div className="col-5">
+
+                    <form>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            placeholder="Add todo..."
+                            name="description"
+                            autoComplete="off"
+                        />
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block mt-2"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
