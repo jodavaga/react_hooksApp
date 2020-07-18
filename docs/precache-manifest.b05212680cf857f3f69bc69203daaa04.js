d13fb5ce01568f5d42cea356e7e00ab23e3cb055
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-17 20:32:05 -0500

    TodoApp build v1

---
+++ docs/precache-manifest.b05212680cf857f3f69bc69203daaa04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a43a1fbadb28edb657126df6d7f34b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "708436d4deaa850428aa",
+    "url": "/static/css/main.ba52f11e.chunk.css"
+  },
+  {
+    "revision": "9609f607b7f67dafefa3",
+    "url": "/static/js/2.86c83d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86c83d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708436d4deaa850428aa",
+    "url": "/static/js/main.5732a7cc.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
