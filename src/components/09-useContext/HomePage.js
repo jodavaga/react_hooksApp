01d6f488769c9ba31dd71b05733428e8c5031827
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-21 18:26:13 -0500

    use of useContext first approach

--- src/components/09-useContext/HomePage.js
+++ src/components/09-useContext/HomePage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomePage = () => {
+
+    const userContext = useContext( UserContext );
+
+    console.log(userContext);
+
     return (
         <div className="container">
             <h1>HomePage</h1>
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1,
+        name: "Jose D. Vasquez",
+        email: "email@email.com"
+    }
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={ user }>
+            <AppRouter />
+
+        </UserContext.Provider>
+
     )
 }
